31629a23dbf70bdd2a0e7f22fa35238f957c52a2
yxc <anyanmolong@qq.com>
2017-03-03 10:27:36 +0800

    提交

--- sample/src/main/java/com/yanzhenjie/swiperecyclerview/base/recyclerview/GlideLoadUrlImageSupport.java
+++ sample/src/main/java/com/yanzhenjie/swiperecyclerview/base/recyclerview/GlideLoadUrlImageSupport.java
@@ -1,9 +1,13 @@
 package com.yanzhenjie.swiperecyclerview.base.recyclerview;
 
+import android.app.Activity;
 import android.content.Context;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
 import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.RequestManager;
 import com.yanzhenjie.recyclerview.swipe.LoadUrlImageSupport;
 
 /**
@@ -18,14 +22,38 @@ public class GlideLoadUrlImageSupport extends LoadUrlImageSupport {
     @Override
     public void loadImageUrl(ImageView view, String url) {
         // 要根据view针对性加载不同大小图片就在这里处理
-        Glide.with(getContext())
-                .load(url)
-                .into(view);
+//        Glide.with(getContext())
+//                .load(url)
+//                .into(view);
+        load(Glide.with(getContext()), view, url);
     }
 
     @Override
     public void loadImageBuilder(ImageView view, String url, Object object) {
+
+        if (object instanceof Activity) {
+            load(Glide.with((Activity) object), view, url);
+            return;
+        }
+        if (object instanceof FragmentActivity) {
+            load(Glide.with((FragmentActivity) object), view, url);
+            return;
+        }
+        if (object instanceof Fragment) {
+            load(Glide.with((Fragment) object), view, url);
+            return;
+        }
+        if (object instanceof android.app.Fragment) {
+            load(Glide.with((android.app.Fragment) object), view, url);
+            return;
+        }
+
         loadImageUrl(view, url);
+    }
+
+    private void load(RequestManager requestManager, ImageView view, String url) {
+        requestManager.load(url)
+                .into(view);
     }
 
     /**
